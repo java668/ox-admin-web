bf321fbed1c90e729a6e924d86217df20c3ddd2d
陈江龙 <java668_real@163.com>
2023-05-15 21:32:00 +0800

    新增登录账号提示

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -43,10 +43,10 @@
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
-      <!--      <div class="tips">-->
-      <!--        <span style="margin-right:20px;">username: admin</span>-->
-      <!--        <span> password: any</span>-->
-      <!--      </div>-->
+            <div class="tips">
+              <span style="margin-right:20px;">账号: admin</span>
+              <span> 密码: 123456【自觉点，莫要乱操作】</span>
+            </div>
 
     </el-form>
   </div>
